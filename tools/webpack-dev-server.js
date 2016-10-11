eedceee7b6a8e6c0f8e1b39910815cbffba1244a
osiyuk <kiril.saradomin@gmail.com>
2016-10-11 15:19:47 +0300

    repair HotModuleReplacement

--- tools/webpack-dev-server.js
+++ tools/webpack-dev-server.js
@@ -2,9 +2,20 @@ import webpackDevServer from 'webpack-dev-server';
 import webpack from 'webpack';
 import config from '../webpack.config.js'
 
+
+Object.keys(config.entry).map( (key) => {
+
+  let entry = config.entry;
+  entry[key] = ['webpack-dev-server/client?http://localhost:8080/',
+    'webpack/hot/dev-server', entry[key]];
+});
+config.plugins.push(new webpack.HotModuleReplacementPlugin());
+
+console.log(config);
+
 const server = new webpackDevServer(webpack(config), {
-  inline: true,
-  hot: true,
+
+  stats: 'error-only',
   publicPath: config.output.publicPath
 });
 server.listen(8080, 'localhost');
