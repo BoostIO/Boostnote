31eacc4b641e4acd2ec0e1b6d63e845bcc22315b
Amir Sharif <budgeneration@gmail.com>
2017-09-23 09:20:59 -0700

    Add prettier to the repo, set it up, and run it on one file.

--- browser/main/modals/PreferencesModal/InfoTab.js
+++ browser/main/modals/PreferencesModal/InfoTab.js
@@ -1,118 +1,154 @@
-import React from 'react'
-import CSSModules from 'browser/lib/CSSModules'
-import styles from './InfoTab.styl'
-import ConfigManager from 'browser/main/lib/ConfigManager'
-import store from 'browser/main/store'
-import AwsMobileAnalyticsConfig from 'browser/main/lib/AwsMobileAnalyticsConfig'
+import React from 'react';
+import CSSModules from 'browser/lib/CSSModules';
+import styles from './InfoTab.styl';
+import ConfigManager from 'browser/main/lib/ConfigManager';
+import store from 'browser/main/store';
+import AwsMobileAnalyticsConfig from 'browser/main/lib/AwsMobileAnalyticsConfig';
 
-const electron = require('electron')
-const { shell, remote } = electron
-const appVersion = remote.app.getVersion()
+const electron = require('electron');
+const { shell, remote } = electron;
+const appVersion = remote.app.getVersion();
 
 class InfoTab extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
-      config: this.props.config
-    }
+      config: this.props.config,
+    };
   }
 
-  handleLinkClick (e) {
-    shell.openExternal(e.currentTarget.href)
-    e.preventDefault()
+  handleLinkClick(e) {
+    shell.openExternal(e.currentTarget.href);
+    e.preventDefault();
   }
 
-  handleConfigChange (e) {
-    const newConfig = { amaEnabled: this.refs.amaEnabled.checked }
+  handleConfigChange(e) {
+    const newConfig = { amaEnabled: this.refs.amaEnabled.checked };
 
-    this.setState({ config: newConfig })
+    this.setState({ config: newConfig });
   }
 
-  handleSaveButtonClick (e) {
-    let newConfig = {
-      amaEnabled: this.state.config.amaEnabled
-    }
+  handleSaveButtonClick(e) {
+    const newConfig = {
+      amaEnabled: this.state.config.amaEnabled,
+    };
 
     if (!newConfig.amaEnabled) {
-      AwsMobileAnalyticsConfig.recordDynamicCustomEvent('DISABLE_AMA')
+      AwsMobileAnalyticsConfig.recordDynamicCustomEvent('DISABLE_AMA');
     }
 
-    ConfigManager.set(newConfig)
+    ConfigManager.set(newConfig);
 
     store.dispatch({
       type: 'SET_CONFIG',
-      config: newConfig
-    })
+      config: newConfig,
+    });
   }
 
-  render () {
+  render() {
     return (
-      <div styleName='root'>
-        <div styleName='header'>Info</div>
-
-        <div styleName='top'>
-          <div styleName='icon-space'>
-            <img styleName='icon' src='../resources/app.png' width='92' height='92' />
-            <div styleName='icon-right'>
-              <div styleName='appId'>Boostnote {appVersion}</div>
-              <div styleName='description'>
-                An open source note-taking app made for programmers just like you.
+      <div styleName="root">
+        <div styleName="header">Info</div>
+        <div styleName="top">
+          <div styleName="icon-space">
+            <img
+              styleName="icon"
+              src="../resources/app.png"
+              width="92"
+              height="92"
+            />
+            <div styleName="icon-right">
+              <div styleName="appId">Boostnote {appVersion}</div>
+              <div styleName="description">
+                An open source note-taking app made for programmers just like
+                you.
               </div>
             </div>
           </div>
         </div>
-        <ul styleName='list'>
+        <ul styleName="list">
           <li>
-            <a href='https://boostnote.io'
-              onClick={(e) => this.handleLinkClick(e)}
-            >Website</a>
-          </li>
-          <li>
-            <a href='https://boostnote.paintory.com/'
-              onClick={(e) => this.handleLinkClick(e)}
-            >Boostnote Shop</a> : Products are shipped to all over the world 🌏
+            <a
+              href="https://boostnote.io"
+              onClick={e => this.handleLinkClick(e)}
+            >
+              Website
+            </a>
           </li>
           <li>
-            <a href='https://salt.bountysource.com/teams/boostnote'
-              onClick={(e) => this.handleLinkClick(e)}
-            >Donate via Bountysource</a> : Thank you for your support 🎉
+            <a
+              href="https://boostnote.paintory.com/"
+              onClick={e => this.handleLinkClick(e)}
+            >
+              Boostnote Shop
+            </a>{' '}
+            : Products are shipped to all over the world 🌏
           </li>
           <li>
-            <a href='https://github.com/BoostIO/Boostnote/issues'
-              onClick={(e) => this.handleLinkClick(e)}
-            >GitHub Issues</a> : We&apos;d love to hear your feedback 🙌
+            <a
+              href="https://salt.bountysource.com/teams/boostnote"
+              onClick={e => this.handleLinkClick(e)}
+            >
+              Donate via Bountysource
+            </a>{' '}
+            : Thank you for your support 🎉
           </li>
           <li>
-            <a href='https://github.com/BoostIO/Boostnote/blob/master/docs/build.md'
-              onClick={(e) => this.handleLinkClick(e)}
-            >Development</a> : Development configurations for Boostnote 🚀
+            <a
+              href="https://github.com/BoostIO/Boostnote/issues"
+              onClick={e => this.handleLinkClick(e)}
+            >
+              GitHub Issues
+            </a>{' '}
+            : We&apos;d love to hear your feedback 🙌
           </li>
-          <li styleName='cc'>
-            Copyright (C) 2017 Maisin&Co.
+          <li>
+            <a
+              href="https://github.com/BoostIO/Boostnote/blob/master/docs/build.md"
+              onClick={e => this.handleLinkClick(e)}
+            >
+              Development
+            </a>{' '}
+            : Development configurations for Boostnote 🚀
           </li>
-          <li styleName='cc'>
-            License: GPL v3
-          </li>
+          <li styleName="cc">Copyright (C) 2017 Maisin&Co.</li>
+          <li styleName="cc">License: GPL v3</li>
         </ul>
         <hr />
-        <div styleName='policy'>Data collection policy</div>
-        <div>We collect only the number of DAU for Boostnote and **DO NOT collect** any detail information such as your note content.</div>
-        <div>You can see how it works on <a href='https://github.com/BoostIO/Boostnote' onClick={(e) => this.handleLinkClick(e)}>GitHub</a>.</div>
-        <div>These data are only used for Boostnote improvements.</div>
-        <input onChange={(e) => this.handleConfigChange(e)}
+        <div styleName="policy">Data collection policy</div>
+        <div>
+          We collect only the number of DAU for Boostnote and **DO NOT collect**
+          any detail information such as your note content.
+        </div>
+        <div>
+          You can see how it works on{' '}
+          <a
+            href="https://github.com/BoostIO/Boostnote"
+            onClick={e => this.handleLinkClick(e)}
+          >
+            GitHub
+          </a>.
+        </div>
+        <div>This data is only used for Boostnote improvements.</div>
+        <input
+          onChange={e => this.handleConfigChange(e)}
           checked={this.state.config.amaEnabled}
-          ref='amaEnabled'
-          type='checkbox'
+          ref="amaEnabled"
+          type="checkbox"
         />
         Enable to send analytics to our servers<br />
-        <button styleName='policy-submit' onClick={(e) => this.handleSaveButtonClick(e)}>Save</button>
+        <button
+          styleName="policy-submit"
+          onClick={e => this.handleSaveButtonClick(e)}
+        >
+          Save
+        </button>
       </div>
-    )
+    );
   }
 }
 
-InfoTab.propTypes = {
-}
+InfoTab.propTypes = {};
 
-export default CSSModules(InfoTab, styles)
+export default CSSModules(InfoTab, styles);
