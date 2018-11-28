045bc9a928ffd37f496f069fff47fc0017ecbc2a
Baptiste Augrain <daiyam@zokugun.org>
2018-11-28 16:47:19 +0100

    fix expanding snippet

--- browser/components/CodeEditor.js
+++ browser/components/CodeEditor.js
@@ -142,6 +142,7 @@ export default class CodeEditor extends React.Component {
 
   updateDefaultKeyMap () {
     const { hotkey } = this.props
+    const expandSnippet = this.expandSnippet.bind(this)
 
     this.defaultKeyMap = CodeMirror.normalizeKeyMap({
       Tab: function (cm) {
@@ -218,7 +219,6 @@ export default class CodeEditor extends React.Component {
 
   componentDidMount () {
     const { rulers, enableRulers } = this.props
-    const expandSnippet = this.expandSnippet.bind(this)
     eventEmitter.on('line:jump', this.scrollToLineHandeler)
 
     const defaultSnippet = [
