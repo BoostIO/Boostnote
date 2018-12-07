36bda966f16a2c89de3468a0717aa8d526a662ef
Pedro Costa <up201605339@fe.up.pt>
2018-12-07 14:23:10 +0000

    allowing styles in html parsing

--- browser/lib/markdown.js
+++ browser/lib/markdown.js
@@ -38,7 +38,7 @@ class Markdown {
       const allowedTags = ['iframe', 'input', 'b',
         'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'br', 'b', 'i', 'strong', 'em', 'a', 'pre', 'code', 'img', 'tt',
         'div', 'ins', 'del', 'sup', 'sub', 'p', 'ol', 'ul', 'table', 'thead', 'tbody', 'tfoot', 'blockquote',
-        'dl', 'dt', 'dd', 'kbd', 'q', 'samp', 'var', 'hr', 'ruby', 'rt', 'rp', 'li', 'tr', 'td', 'th', 's', 'strike', 'summary', 'details'
+        'dl', 'dt', 'dd', 'kbd', 'q', 'samp', 'var', 'hr', 'ruby', 'rt', 'rp', 'li', 'tr', 'td', 'th', 's', 'strike', 'summary', 'style', 'details'
       ]
       const allowedAttributes = [
         'abbr', 'accept', 'accept-charset',
@@ -55,15 +55,10 @@ class Markdown {
         'nowrap', 'open', 'prompt', 'readonly', 'rel', 'rev',
         'rows', 'rowspan', 'rules', 'scope',
         'selected', 'shape', 'size', 'span',
-        'start', 'summary', 'tabindex', 'target',
+        'start', 'summary', 'style', 'tabindex', 'target',
         'title', 'type', 'usemap', 'valign', 'value',
         'vspace', 'width', 'itemprop'
       ]
-
-      if (updatedOptions.sanitize === 'ALLOW_STYLES') {
-        allowedTags.push('style')
-        allowedAttributes.push('style')
-      }
 
       // Sanitize use rinput before other plugins
       this.md.use(sanitize, {
