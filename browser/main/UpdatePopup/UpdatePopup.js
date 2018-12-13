cc323e7bbf1947aaf8c702d7c523c592ea4afa8f
Sandro Campos <up201605947@fe.up.pt>
2018-12-13 16:02:14 +0000

    code styling correction

--- browser/main/UpdatePopup/UpdatePopup.js
+++ browser/main/UpdatePopup/UpdatePopup.js
@@ -6,38 +6,33 @@ const electron = require('electron')
 const { remote, ipcRenderer } = electron
 const { dialog } = remote
 
-function updateApp() {
+function updateApp () {
+  const config = ConfigManager.get()
 
-    const config = ConfigManager.get()
+  if (config.ui.updateNotification) {
+    const index = dialog.showMessageBox(remote.getCurrentWindow(), {
+      type: 'warning',
+      message: i18n.__('Update Boostnote'),
+      detail: i18n.__('New Boostnote is ready to be installed.'),
+      buttons: [i18n.__('Restart & Install'), i18n.__('Not Now'), i18n.__('Not Show Again')]
+    })
 
-    if (config.ui.updateNotification) {
+    if (index === 0) {
+      ipcRenderer.send('update-app-confirm')
+    } else if (index === 2) {
+      const newConfig = Object.assign({}, config)
+      newConfig.ui.updateNotification = false
 
-        const index = dialog.showMessageBox(remote.getCurrentWindow(), {
-            type: 'warning',
-            message: i18n.__('Update Boostnote'),
-            detail: i18n.__('New Boostnote is ready to be installed.'),
-            buttons: [i18n.__('Restart & Install'), i18n.__('Not Now'), i18n.__('Not Show Again')]
-        })
+      ConfigManager.set(newConfig)
 
-        if (index === 0) {
-            ipcRenderer.send('update-app-confirm')
-        }
-        else if (index === 2) {
-
-            const newConfig = Object.assign({}, config)
-            newConfig.ui.updateNotification = false;
-
-            ConfigManager.set(newConfig)
-
-            store.dispatch({
-                type: 'SET_UI',
-                config: newConfig
-            })
-        }
+      store.dispatch({
+        type: 'SET_UI',
+        config: newConfig
+      })
     }
+  }
 }
 
-
 export default {
-    updateApp
+  updateApp
 }
