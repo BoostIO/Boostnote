387f19461b162604535f8cf1be524e3105960b97
Berto Moore <30520062+bertomoore@users.noreply.github.com>
2020-05-06 23:12:24 -0600

    Update browser/main/modals/PreferencesModal/BackupTab.js
    Co-authored-by: Nguyen Viet Hung <viethungax@gmail.com>

--- browser/main/modals/PreferencesModal/BackupTab.js
+++ browser/main/modals/PreferencesModal/BackupTab.js
@@ -233,7 +233,7 @@ class BackupTab extends React.Component {
           <div styleName='group-section'>
             <div styleName='backup-body-section'>
               <div styleName='backup-body-section-label'>
-                {i18n.__('Create Backup File')}
+                {i18n.__('Export Backup File')}
               </div>
               <div styleName='backup-body-section-path'>
                 <input
