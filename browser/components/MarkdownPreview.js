f3957a79a36515faf5e3ee56bbef058c4fee0476
Julian <me@julian.pro>
2020-09-19 07:22:18 +0200

    Update MarkdownPreview.js
    according to CodeReview in PR #3625 removed comments

--- browser/components/MarkdownPreview.js
+++ browser/components/MarkdownPreview.js
@@ -89,7 +89,6 @@ function escapeRegExp(string) {
   return string.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&') // $& means the whole matched string
 }
 
-// replaceAll replaces all occurences
 function replaceAll(str, find, replace) {
   return str.replace(new RegExp(escapeRegExp(find), 'g'), replace)
 }
